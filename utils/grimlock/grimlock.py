b20c35dba37bd18afc2da159e7721e9669455259
Amadeusz Walag <Amadeusz.Walag@medapp.pl>
2025-09-02 22:42:29 +0200

    cleaning code

--- utils/grimlock/grimlock.py
+++ utils/grimlock/grimlock.py
@@ -18,20 +18,21 @@ MASTER_PASSWORD_HASH = None
 ENCRIPTION_LENGTH = None
 SALT_LENGTH = None
 NONCE_LENGTH = None
+MAX_PASSWORD_LENGTH = 32
 RETURN_TO_MENU_INFO_TEXT = "Press Enter to return to menu..."
 VAULT_PATH = "vault.json"
 
-def _b64e(b: bytes) -> str:
+def _b64e(b : bytes) -> str:
     return base64.b64encode(b).decode()
 
-def _b64d(s: str) -> bytes:
+def _b64d(s : str) -> bytes:
     return base64.b64decode(s.encode())
 
 def return_to_menu(s : str):
     if G_cli_mode == False:
         input(RETURN_TO_MENU_INFO_TEXT)
 
-def derive_key(master_password: str, salt: bytes) -> bytes:
+def derive_key(master_password : str, salt : bytes) -> bytes:
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
         length=ENCRIPTION_LENGTH,
@@ -41,7 +42,7 @@ def derive_key(master_password: str, salt: bytes) -> bytes:
 
     return kdf.derive(master_password.encode())
 
-def encrypt_password(plain_text: str, master_password: str) -> dict:
+def encrypt_password(plain_text : str, master_password : str) -> dict:
     salt = os.urandom(SALT_LENGTH)
     key = derive_key(master_password, salt)
     aesgcm = AESGCM(key)
@@ -54,13 +55,13 @@ def encrypt_password(plain_text: str, master_password: str) -> dict:
         "ct": _b64e(ciphertext),
     }
 
-def decrypt_password(bundle: dict, master_password: str) -> str:
+def decrypt_password(bundle : dict, master_password : str) -> str:
     key = derive_key(master_password, _b64d(bundle["salt"]))
     aesgcm = AESGCM(key)
     plain = aesgcm.decrypt(_b64d(bundle["nonce"]), _b64d(bundle["ct"]), None)
     return plain.decode()
 
-def store_credentials(account: str, bundle: dict):
+def store_credentials(account : str, bundle : dict):
     data = {}
 
     if os.path.exists(VAULT_PATH):
@@ -70,13 +71,12 @@ def store_credentials(account: str, bundle: dict):
             except json.JSONDecodeError:
                 data = {}
 
-    #CHECK IF WE ARE OVERRIDEING DATA
     data[account] = bundle
 
     with open(VAULT_PATH, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=2)
 
-def load_credentials(account: str) -> dict | None:
+def load_credentials(account : str) -> dict | None:
     data = {}
 
     if not os.path.exists(VAULT_PATH):
@@ -89,7 +89,7 @@ def load_credentials(account: str) -> dict | None:
     
     return data.get(account)
 
-def remove_credentials(account: str):
+def remove_credentials(account : str):
     data = {}
 
     if os.path.exists(VAULT_PATH):
@@ -119,6 +119,7 @@ def load_configuration():
         ENCRIPTION_LENGTH = int(config_data['config']['encription_length'])
         SALT_LENGTH = int(config_data['config']['salt_lenth'])
         NONCE_LENGTH = int(config_data['config']['nonce'])
+        MAX_PASSWORD_LENGTH = int(config_data['config']['max_password_length'])
 
 def check_master_password():
     global G_authenticated, G_session_password, G_cli_password
@@ -161,7 +162,87 @@ def get_accounts() -> list | None:
 
     return list(data.keys())
 
-def save_new_password():
+def check_if_account_exists(account_name : str) -> bool:
+    saved_accounts = get_accounts()
+
+    if account_name in saved_accounts:
+        print("Account name already saved by the manager")
+        return False
+    return True
+
+def parse_account_name(account_name : str) -> bool:
+    if not all(ch.isalnum() or ch.isspace() for ch in account_name):
+        print("Account name can only contain letters, numbers, and spaces")
+        return False
+
+    if not account_name:
+        print("The account name cannot be empty")
+        return False
+
+    return True
+
+def parse_password(password : str) -> bool:
+    if " " in password or "\t" in password or "\n" in password:
+        print("Password can't contain whilespaces")
+        return False
+
+    if len(password) > MAX_PASSWORD_LENGTH:
+        print(f"Password cannout exceed { MAX_PASSWORD_LENGTH } characters")
+        return False
+
+    return True
+
+def get_account_anme_and_password_from_line(line : str):
+    try:
+        account_name, password = line.split(",", 1)
+        account_name = account_name.strip()
+        password = password.strip()
+
+        if not parse_account_name(account_name):
+            account_name = None
+        
+        if not parse_password(password):
+            password = None
+
+        return (account_name, password)
+    except ValueError:
+        print(f"Account name and password must be comma separated")
+
+    return (None, None)
+
+def read_accounts_from_file(file : str):
+    account_file_content = None
+
+    try:
+        with open(file, "r") as f:
+            account_file_content = f.read()
+    except FileNotFoundError:
+        print(f"File { file } was not found")
+        return None
+    except Exception as e:
+        print(f"An unexpected error has occured: { e }")
+        return None
+    
+    return account_file_content
+
+def list_accounts() -> bool:
+    saved_accounts = get_accounts()
+
+    if saved_accounts == None:
+        print("No accounts found")
+        return False
+
+    print("\n=== Stored Accounts ===")
+    for i, acc in enumerate(saved_accounts, start=1):
+        print(f"[{i}] {acc}")
+    return True
+
+def save_new_password(account_name : str, account_password : str):
+    bundle = encrypt_password(account_password, G_session_password)
+    store_credentials(account_name, bundle)
+    print("Password saved")
+
+def save_new_password_visual_mode():
     clear_screen()
 
     if not check_master_password():
@@ -169,27 +250,34 @@ def save_new_password():
         return
 
     account_name = input("Account / service name: ").strip()
-    if not account_name:
-        print("Account name cannot be empty")
-        return_to_menu()
+    if not parse_account_name(account_name):
         return
 
-    saved_accounts = get_accounts()
-
-    if account_name in saved_accounts:
-        print("Account alread has a password")
-        return_to_menu()
+    if check_if_account_exists(account_name):
         return
 
-    account_password = getpass.getpass(f"Password for '{account_name}': ")
-    bundle = encrypt_password(account_password, G_session_password)
-    store_credentials(account_name, bundle)
+    account_password = getpass.getpass(f"Password for '{ account_name }': ")
 
-    print("Password saved")
+    if not parse_password(account_password):
+        return
+
+    save_new_password(account_name, account_password)
 
     return_to_menu()
 
-def get_password():
+def get_password(account_name : str):
+    bundle = load_credentials(account_name)
+
+    if bundle == None:
+        print(f"Credentials for {account_name} were not found")
+        return_to_menu()
+
+    selected_account_password = decrypt_password(bundle, G_session_password)
+
+    pyperclip.copy(selected_account_password)
+    print("Password copied to clipboard")
+
+def get_password_visual_mode():
     clear_screen()
 
     if not check_master_password():
@@ -198,17 +286,12 @@ def get_password():
 
     saved_accounts = get_accounts()
 
-    if saved_accounts == None:
-        print("No account list found")
+    if not list_accounts():
         return_to_menu()
         return
 
-    print("\n=== Stored Accounts ===")
-    for i, acc in enumerate(saved_accounts, start=1):
-        print(f"[{i}] {acc}")
-
     num_of_accounts = len(saved_accounts)
-    selected_account = input(f"Choose account from [1] to [{num_of_accounts}]: ")
+    selected_account = input(f"Choose account from [1] to [{ num_of_accounts }]: ")
 
     if not selected_account.isnumeric():
         print("Input was not a number")
@@ -223,20 +306,11 @@ def get_password():
         return
 
     selected_account_name = saved_accounts[selected_account - 1]
-    bundle = load_credentials(selected_account_name)
-
-    if bundle == None:
-        print(f"Credentials for {selected_account_name} were not found")
-        return_to_menu()
-
-    selected_account_password = decrypt_password(bundle, G_session_password)
-
-    pyperclip.copy(selected_account_password)
-    print("Password copied to clipboard")
+    get_password(selected_account_name)
 
     return_to_menu()
 
-def remove_password():
+def remove_password_visual_mode():
     clear_screen()
     print("REMOVING ACCOUNT!")
 
@@ -263,6 +337,9 @@ def remove_password():
     if confirmation.lower() == "y":
         remove_credentials(account_name)
 
+    print(f"Account { account_name } has been deleted")
+
+
 def visual_mode():
     while True:
         clear_screen()
@@ -275,11 +352,11 @@ def visual_mode():
         menu_option = input("Choose an option [1-4]: ").strip()
 
         if menu_option == "1":
-              save_new_password()
+              save_new_password_visual_mode()
         elif menu_option == "2":
-              get_password()
+            get_password_visual_mode()
         elif menu_option == "3":
-            remove_password()
+            remove_password_visual_mode()
         elif menu_option == "4":
             print("Exiting...")
             sys.exit(0)
@@ -305,100 +382,58 @@ def cli_mode():
         if not check_master_password():
             return
     else:
-        print("error: -mp argument was not provided")
+        print("Error: -mp argument was not provided")
         return
 
     if args.ls:
-        saved_accounts = get_accounts()
-
-        if saved_accounts == None:
-            print("No account list found")
-            return
-
-        print("=== Stored Accounts ===")
-        for _, acc in enumerate(saved_accounts):
-            print(f"- {acc}")
+        list_accounts()
         return
     elif args.g:
         account_name = args.g
-        saved_accounts = get_accounts()
-        
-        if account_name not in saved_accounts:
-            print(f"Account { account_name } was not found")
-            return
-
-        bundle = load_credentials(account_name)
-
-        if bundle == None:
-            print(f"Credentials for {selected_account_name} were not found")
-            return_to_menu()
-
-        account_password = decrypt_password(bundle, G_session_password)
-
-        pyperclip.copy(account_password)
-        print("Password copied to clipboard")
+        get_password(account_name)
         return
     elif args.n:
         try:
             account_name, password = args.n.split(",", 1)
         except ValueError:
-            print("-n argument must contain a comma separated account name and password")
+            print("Error: -n argument must contain a comma separated account name and password")
             return
 
         account_name = account_name.strip()
         password = password.strip()
 
-        if not all(ch.isalnum() or ch.isspace() for ch in account_name):
-            print("account name can only contain letters, numbers, and spaces")
-
-        if not account_name:
-            print("the account name cannot be empty")
+        if not parse_account_name(account_name):
             return
 
-        saved_accounts = get_accounts()
-
-        if account_name in saved_accounts:
-            print("account with this name alread is saved in the manager")
+        if check_if_account_exists():
             return
 
-        if " " in password or "\t" in password or "\n" in password:
-            print("password can't contain whilespaces")
-            return
-        if len(password) > 64:
-            print("password cannout exceed 64 characters")
+        if not parse_password(password):
             return
 
-        bundle = encrypt_password(password, G_session_password)
-        store_credentials(account_name, bundle)
-
-        print("Password saved")
+        save_new_password(account_name, password)
         return
     elif args.r:
-        account_to_remove = args.r.strip()
+        account_name = args.r.strip()
         saved_accounts = get_accounts()
 
         if saved_accounts == None:
             print("No accounts found")
             return
     
-        if account_to_remove not in saved_accounts:
-            print(f"Account { account_to_remove } not found")
+        if account_name not in saved_accounts:
+            print(f"Account { account_name } not found")
             return
 
-        remove_credentials(account_to_remove)
-        print(f"account { account_to_remove } has been deleted")
+        remove_credentials(account_name)
+        print(f"Account { account_name } has been deleted")
         return
     elif args.fn:
         account_file = args.fn
-        account_file_content = {}
+        account_file_content = read_accounts_from_file(account_file)
 
-        try:
-            with open(account_file, "r") as file:
-                account_file_content = file.read()
-        except FileNotFoundError:
-            print(f"File { account_file } was not found.")
-        except Exception as e:
-            print(f"An unexpected error has occured: { e }")
+        if account_file_content == None:
+            return
 
         loaded_account_count = 0
         success_count = 0
@@ -406,38 +441,16 @@ def cli_mode():
         for account_number, account in enumerate(account_file_content.split("\n"), start=1):
             loaded_account_count += 1
 
-            try:
-                account_name, password = account.split(",", 1)
-            except ValueError:
-                print(f"Account name and password must be comma separated. Account line { account_number }")
+            account_name, password = get_account_anme_and_password_from_line(account)
+
+            if account_name == None or password == None:
+                print(f"Account line { account_number }")
                 continue
 
-            account_name = account_name.strip()
-            password = password.strip()
+            if check_if_account_exists(account_name):
+                print(f"Account line { account_number }")
 
-            if not all(ch.isalnum() or ch.isspace() for ch in account_name):
-               print(f"Account name can only contain letters, numbers, and spaces. Account line { account_number }")
-               continue
-
-            if not account_name:
-                print(f"The account name cannot be empty. Account line { account_number }")
-                continue
-
-            saved_accounts = get_accounts()
-
-            if account_name in saved_accounts:
-                print(f"Account with this name alread is saved in the manager. Account line { account_number }")
-                continue
-
-            if " " in password or "\t" in password or "\n" in password:
-                print(f"Password can't contain whilespaces. Account line { account_number }")
-                continue
-            if len(password) > 64:
-                print(f"Password cannout exceed 64 characters. Account line { account_number }")
-                continue
-
-            bundle = encrypt_password(password, G_session_password)
-            store_credentials(account_name, bundle)
+            save_new_password(account_name, password)
             success_count += 1
         
         print(f"{ success_count }/{ loaded_account_count } accounts saved")
